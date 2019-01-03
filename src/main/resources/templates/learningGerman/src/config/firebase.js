2e85a26dabd8f516b3c1c6fdf32d79becf49c1c9
h5dde45 <tmvf@yandex.ru>
2019-01-03 21:45:04 +0300

    Delete firebase.js

--- src/main/resources/templates/learningGerman/src/config/firebase.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyB9sRnHcbYIT_uG3NAjWsYDi-J0wXBg5y0",
-    authDomain: "learninggerman.firebaseapp.com",
-    databaseURL: "https://learninggerman.firebaseio.com",
-    projectId: "learninggerman",
-    storageBucket: "learninggerman.appspot.com",
-    messagingSenderId: "4018993125"
-}
