8860a0e2b244efd421a495490f4f7530bebabb68
tmvf <tmvf@yandex.ru>
2019-01-03 16:45:43 +0400

    18

---
+++ src/main/resources/templates/learningGerman/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app>
+        <app-header></app-header>
+        <v-content>
+            <router-view/>
+        </v-content>
+        <app-footer></app-footer>
+    </v-app>
+</template>
+<script>
+    import appHeader from "./components/AppHeader.vue"
+    import appFooter from "./components/AppFooter.vue"
+    export default{
+        components: {
+            appHeader,
+            appFooter
+        }
+    }
+</script>
+<style>
+</style>
