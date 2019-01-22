876c7d6f7451d64e23758db0bb7b9c553d40a0a4
tmvf <tmvf@yandex.ru>
2019-01-22 15:42:45 +0400

    28

---
+++ src/main/resources/templates/simple_2/src/greetings.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>hi, {{name}} </h1>
+    <button @click="changeName">Not {{name}}</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: 'asd'
+      }
+    },
+    methods: {
+      changeName(){
+        this.name = this.name === 'asd' ? 'zxc' : 'asd'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/main/resources/templates/simple_2/src/header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div >
+    <h1>Header </h1>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+ div{
+  border: 3px solid goldenrod;
+}
+</style>
