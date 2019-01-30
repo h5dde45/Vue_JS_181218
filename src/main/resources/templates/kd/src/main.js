161eae64e8e16c01768d5e04589ba51e7a928567
tmvf <tmvf@yandex.ru>
2019-01-30 23:26:22 +0400

    36

--- src/main/resources/templates/kd/src/main.js
+++ src/main/resources/templates/kd/src/main.js
@@ -1,13 +1,15 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
-import App from "./p05/App.vue";
-import router from "./p05/routes";
+import VueResource from "vue-resource";
+import App from "./p06/App.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueResource)
+Vue.http.options.root = 'http://localhost:3000/'
+Vue.http.interceptors.push(request => {
+  request.headers.set('Auth', 'rand token' + Math.random())
+})
 
 new Vue({
   el: '#app',
-  router,
   render: h => h(App)
 })
 
---
+++ src/main/resources/templates/kd/src/p06/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container pt-2">
+    <div class="form-group">
+      <label for="name">Car name</label>
+      <input type="text" id="name"
+             class="form-control"
+             v-model.trim="carName">
+    </div>
+    <div class="form-group">
+      <label for="year">Car year</label>
+      <input type="text" id="year"
+             class="form-control"
+             v-model.number="carYear">
+    </div>
+    <button class="btn btn-info" @click="createCar">Create car</button>
+    <button class="btn btn-primary" @click="loadCars">Load cars</button>
+    <hr>
+    <ul class="list-group">
+      <li class="list-group-item"
+          v-for="car in cars"
+          :key="car.id">
+        <strong>{{car.name}}</strong> - {{car.year}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data(){
+      return {
+        carName: '',
+        carYear: 2111,
+        cars: [],
+        resource: null
+      }
+    },
+    methods: {
+      createCar(){
+        const car = {
+          name: this.carName,
+          year: this.carYear
+        }
+        this.resource.save({}, car)
+
+      },
+      loadCars(){
+        this.resource.get()
+          .then(response => {
+            return response.json()
+          })
+          .then(cars => {
+            this.cars = cars
+          })
+      }
+    },
+    created(){
+      this.resource = this.$resource('cars')
+    }
+  }
+</script>
+
+<style>
+  body {
+    background: darkgrey;
+  }
+</style>
